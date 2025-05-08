b5adf62139e2edef0d795197fb64373082f78c62
milan <milan23@mhs.usk.ac.id>
2025-05-09 01:27:07 +0700

    report router update

--- routes/reportRouter.js
+++ routes/reportRouter.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/createReport", createReport);
 router.put("/approve/:reportId", approveReport);
 router.put("/reject/:reportId", rejectReport);
+router.get("/getAllReport", getAllReport);
 
 export default router;
