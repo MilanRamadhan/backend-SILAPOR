daf2d4e2de54ba7439cb7124172894f74a735e0e
milan <milan23@mhs.usk.ac.id>
2025-05-12 01:16:53 +0700

    update middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import Auth from "../models/Auth";
+import Auth from "../models/Auth.js";
 
 export const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -11,13 +11,13 @@ export const verifyToken = async (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
-    // Ambil auth berdasarkan ID dari token
-    const auth = await Auth.findById(decoded.id);
-    if (!auth) {
+    // Ambil user berdasarkan ID dari token
+    const user = await Auth.findById(decoded.id);
+    if (!user) {
       return res.status(401).json({ message: "User tidak ditemukan" });
     }
 
-    req.auth = auth; // auth lengkap dimasukkan ke req.auth
+    req.user = user; // user lengkap dimasukkan ke req.user
     next();
   } catch (err) {
     return res.status(401).json({ message: "Token tidak valid atau sudah kedaluwarsa" });
