c0c9bbd80fd021d021c507519262737a0143d5c2
milan <milan23@mhs.usk.ac.id>
2025-05-14 22:04:30 +0700

    update authrouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,8 +5,8 @@ const authRouter = express.Router();
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
-authRouter.put("/updateUser/:userId", updateUser);
-authRouter.post("/updatePassword/:userId", updatePassword);
+authRouter.put("/updateUser/:id", updateUser);
+authRouter.post("/updatePassword/:id", updatePassword);
 authRouter.get("/getAllUsers", getAllUsers);
 authRouter.put("/changeProfilePhoto", changeProfilePhoto);
 
