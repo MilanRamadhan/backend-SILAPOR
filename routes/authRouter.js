d883dcd6b9dbc053556fccbcc16495c6deedc735
milan <milan23@mhs.usk.ac.id>
2025-05-15 04:18:35 +0700

    update auth router

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express from "express";
 import { register, login, updateUser, updatePassword, changeProfilePhoto, getAllUsers, logout } from "../controllers/authController.js";
 
 const authRouter = express.Router();
